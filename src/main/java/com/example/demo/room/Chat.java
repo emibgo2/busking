543704b3aeb982c47a0e39efe29e23317679bb5b
emibgo2 <emibgo2@naver.com>
2021-11-14 14:33:58 +0900

    Heroku upload test ( Chat class delete )
    Busking Project Start + 31
    Spring Boot Start D+137

--- src/main/java/com/example/demo/room/Chat.java
+++ src/main/java/com/example/demo/room/Chat.java
@@ -1,41 +1,41 @@
-package com.example.demo.room;
-
-import com.example.demo.user.User;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-
-import javax.persistence.*;
-import java.sql.Timestamp;
-
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-@Entity
-public class Chat {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false,length = 200)
-    private String content;
-
-    @ManyToOne
-    @JoinColumn(name = "roomId")
-    private Room chatRoom;
-
-    @ManyToOne
-    @JoinColumn(name ="nickname")
-    private User user;
-
-
-
-    @CreationTimestamp
-    private Timestamp createDate;
-
-
-}
+//package com.example.demo.room;
+//
+//import com.example.demo.user.User;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import org.hibernate.annotations.CreationTimestamp;
+//
+//import javax.persistence.*;
+//import java.sql.Timestamp;
+//
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+//@Entity
+//public class Chat {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(nullable = false,length = 200)
+//    private String content;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "roomId")
+//    private Room chatRoom;
+//
+//    @ManyToOne
+//    @JoinColumn(name ="nickname")
+//    private User user;
+//
+//
+//
+//    @CreationTimestamp
+//    private Timestamp createDate;
+//
+//
+//}
